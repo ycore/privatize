f4d5dc88a94f69539683ac5b744514b8822cda1d
Johan Meyer <y-core@outlook.com>
2025-02-27 13:30:31 +0200

    Read from packagae.json provatize key

--- index.mjs
+++ index.mjs
@@ -12,66 +12,69 @@
 //  3.1 on local machine, update ENV DEPLOYMENT_KEY= with 'the base64 private key content' in Node.dockerfile
 //  3.2 on cloudflare, Update DEPLOYMENT_KEY environment variable with 'the base64 private key content' in both production and preview environments
 
-import { execSync } from 'child_process';
-import { env, exit } from 'node:process';
-import { readFileSync, existsSync } from 'fs';
-import path from 'path';
+import { execSync } from "node:child_process";
+import { existsSync, readFileSync } from "node:fs";
+import path from "node:path";
+import { env, exit } from "node:process";
+
+const SSH_PATH = "~/.ssh/id_ed25519";
 
 function main() {
-  if (env.PRIVATIZE_INSTALL) {
-    console.info('Status: PRIVATIZE_INSTALL completed previously.');
-    exit(0);
-  }
+	if (!env.DEPLOYMENT_KEY) {
+		console.error(
+			"Privatize error: encoded DEPLOYMENT_KEY environment variable is not set.",
+		);
+		exit(1);
+	}
 
-  const jsonPath = path.resolve(process.cwd(), 'privatize.json');
+	const packageName = path.resolve(process.cwd(), "package.json");
+	if (!existsSync(packageName)) {
+		console.error("Privatize error: package.json not found.");
+		exit(1);
+	}
 
-  if (!existsSync(jsonPath)) {
-    console.info('No privatize.json file found in application root.');
-    exit(0);
-  }
+	const packageJson = JSON.parse(readFileSync(packageName, "utf-8"));
+	if (!Object.hasOwn(packageJson, "privatize")) {
+		console.info("No privatize key found");
+		exit(0);
+	}
 
-  const data = readFileSync(jsonPath, 'utf8');
-  const { packages } = JSON.parse(data);
+	const repositories = Object.values(packageJson.privatize).join(" ");
+	if (!repositories || repositories.length === 0) {
+		console.info("No privatize repositories found");
+		exit(0);
+	}
 
-  if (!env.DEPLOYMENT_KEY) {
-    console.error(
-      'Error: encoded DEPLOYMENT_KEY environment variable is not set.'
-    );
-    exit(1);
-  }
+	// Create the .ssh directory and add GitHub to known hosts
+	try {
+		execSync(
+			"mkdir -p ~/.ssh && ssh-keyscan -Ht rsa github.com >> ~/.ssh/known_hosts",
+			{ stdio: "inherit" },
+		);
+		// Decode the ENV_DEPLOYMENT_KEY and save it to a file
+		execSync(`echo ${env.DEPLOYMENT_KEY} | base64 -d > ${SSH_PATH}`, {
+			stdio: "inherit",
+		});
+		execSync(`chmod 600 ${SSH_PATH}`, { stdio: "inherit" });
 
-  // Create the .ssh directory and add GitHub to known hosts
-  try {
-    execSync(
-      'mkdir -p ~/.ssh && ssh-keyscan -Ht rsa github.com >> ~/.ssh/known_hosts',
-      { stdio: 'inherit' }
-    );
+		console.log("SSH environment ready for github private install.");
+	} catch (error) {
+		console.error("Error during SSH environment setup:", error.message);
+		exit(1);
+	}
 
-    // Decode the ENV_DEPLOYMENT_KEY and save it to a file
-    execSync(`echo ${env.DEPLOYMENT_KEY} | base64 -d > ~/.ssh/id_ed25519`, {
-      stdio: 'inherit',
-    });
-    execSync('chmod 600 ~/.ssh/id_ed25519', { stdio: 'inherit' });
-
-    console.log('SSH environment ready for github private install.');
-  } catch (error) {
-    console.error('Error during SSH environment setup:', error.message);
-    exit(1);
-  }
-
-  // Run npm install without scripts
-  try {
-    execSync(`npm install ${packages.join(' ')} --no-scripts --save-optional`, {
-      stdio: 'inherit',
-    });
-    //   execSync('rm -rf ~/.ssh', { stdio: 'inherit' });
-
-    env.PRIVATIZE_INSTALL = 'COMPLETE';
-    console.log('privatize install completed successfully.');
-  } catch (error) {
-    console.error('Error during npm install:', error.message);
-    exit(1);
-  }
+	// Run npm install without scripts
+	try {
+		console.log(`npm install ${repositories} --no-scripts --save-optional`);
+		execSync(`npm install ${repositories} --no-scripts --save-optional`, {
+			stdio: "inherit",
+		});
+		execSync(`rm -rf ${SSH_PATH}`, { stdio: "inherit" });
+		console.log("privatize install completed successfully.");
+	} catch (error) {
+		console.error("Error during npm install:", error.message);
+		exit(1);
+	}
 }
 
 main();
